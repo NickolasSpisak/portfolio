f07faaa961d970d485d1d1fa8392bd48aae7f6bf
spish-web <nickolas.spisak@gmail.com>
2022-02-27 00:59:31 -0500

    fixed weird npm problem

--- client/src/componants/footer/footerComponant.js
+++ client/src/componants/footer/footerComponant.js
@@ -1,7 +1,7 @@
 import { Component } from "react";
-// import { faGithub, faAmazon } from "@fortawesome/free-brands-svg-icons";
+import { faGithub, faAmazon } from "@fortawesome/free-brands-svg-icons";
 
-// import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import "./footer.css";
 class Footer extends Component {
   render() {
@@ -10,13 +10,13 @@ class Footer extends Component {
         <div className="container">
           <div className="row">
             <div className="col mt-5">
-              {/* <FontAwesomeIcon icon={faGithub} /> */}
+              <FontAwesomeIcon icon={faGithub} />
               <a href="https://github.com/NickolasSpisak">My Github</a>
             </div>
           </div>
           <div className="row">
             <div className="col mb-5">
-              {/* <FontAwesomeIcon icon={faAmazon} /> */}
+              <FontAwesomeIcon icon={faAmazon} />
               <a href="https://www.amazon.com/Darkest-Side-Moon-Nick-Spisak/dp/1984543598">
                 Darkest Side of the Moon
               </a>
