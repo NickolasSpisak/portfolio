efbf2895a4f7cafefa4ff2fc15983171d2326bb1
spish-web <nickolas.spisak@gmail.com>
2022-03-26 16:27:16 -0400

    changed background

--- client/src/componants/developer/developerComponent.js
+++ client/src/componants/developer/developerComponent.js
@@ -13,16 +13,32 @@ class Developer extends Component {
           </div>
           <div className="row">
             <div className="col mb-5">
-              <h1 className="website">SpishStories.com</h1>
-              <a href="http://spishstories.com">
+              <h1 className="website">Posts App</h1>
+              <a href="https://nickspisakposts.netlify.app">
                 <img
                   className="spishstories"
-                  src="/images/spishstories.png"
-                  alt="spishstories"
+                  src="/images/posts.png"
+                  alt="posts"
                   height={400}
                   width={500}
                 />
               </a>
+            </div>
+            <div className="col mt-5">
+              <p className="text">
+                A responsive post application using React, MongoDB, Express,
+                Node.js. Responsive account creation, as well as Google sign in.
+                Each user can create and delete their own posts. A Like button
+                is currently in development!
+              </p>
+            </div>
+            <div className="col-6 mt-5">
+              <p className="text">
+                Educational React application that helps teach the large scale
+                of the solar system. The length of scrolling is the same amount
+                of time it takes light to travel. Mobile version and more
+                effecient rendering is in development!
+              </p>
             </div>
             <div className="col">
               <h1 className="website">Scrollable Solar System</h1>
@@ -40,16 +56,23 @@ class Developer extends Component {
           </div>
           <div className="row">
             <div className="col">
-              <h1 className="website">Posts App</h1>
-              <a href="https://nickspisakposts.netlify.app">
+              <h1 className="website">SpishStories.com</h1>
+              <a href="http://spishstories.com">
                 <img
                   className="spishstories"
-                  src="/images/posts.png"
-                  alt="posts"
+                  src="/images/spishstories.png"
+                  alt="spishstories"
                   height={400}
                   width={500}
                 />
               </a>
+            </div>
+            <div className="col mt-5">
+              <p className="text">
+                React application which hosts stories I have written. Currently
+                using React, CSS, and HTML. A responsive server is in
+                development for a responsive webcomic reader!
+              </p>
             </div>
           </div>
         </div>
