4cdb2daae6dedd8ee9c1b07ab2c205820ebb1984
spish-web <nickolas.spisak@gmail.com>
2022-03-26 20:11:17 -0400

    added responsive cards

--- client/src/componants/developer/developerComponent.js
+++ client/src/componants/developer/developerComponent.js
@@ -1,7 +1,25 @@
 import { Component } from "react";
+import { Card, CardBody, CardHeader, Collapse, Button } from "reactstrap";
 
 import "./developer.css";
 class Developer extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      collapse: null,
+    };
+    this.handleOpenCard = this.handleOpenCard.bind(this);
+    this.handleCloseCard = this.handleCloseCard.bind(this);
+  }
+  handleOpenCard(val) {
+    this.setState({ activeCard: val });
+    this.setState({ collapse: true });
+  }
+  handleCloseCard() {
+    this.setState({ showCard: false });
+    this.setState({ showCard: "" });
+  }
+
   render() {
     return (
       <div className="developer">
@@ -14,65 +32,94 @@ class Developer extends Component {
           <div className="row">
             <div className="col mb-5">
               <h1 className="website">Posts App</h1>
-              <a href="https://nickspisakposts.netlify.app">
+              <Button outline onClick={() => this.toggle}>
                 <img
-                  className="spishstories"
                   src="/images/posts.png"
                   alt="posts"
                   height={400}
                   width={500}
+                  onClick={this.handleOpenCard}
                 />
-              </a>
+              </Button>
             </div>
             <div className="col mt-5">
-              <p className="text">
-                A responsive post application using React, MongoDB, Express,
-                Node.js. Responsive account creation, as well as Google sign in.
-                Each user can create and delete their own posts. A Like button
-                is currently in development!
-              </p>
+              <Collapse isOpen={this.state.collapse}>
+                <Card className="card">
+                  <a href="https://nickspisakposts.netlify.app">
+                    <CardBody>
+                      <p className="cardText">
+                        A responsive post application using React, MongoDB,
+                        Express, Node.js. Responsive account creation, as well
+                        as Google sign in. Each user can create and delete their
+                        own posts. A Like button is currently in development!
+                      </p>
+
+                      <p className="cardText">Click to visit!</p>
+                    </CardBody>
+                  </a>
+                </Card>
+              </Collapse>
             </div>
             <div className="col-6 mt-5">
-              <p className="text">
-                Educational React application that helps teach the large scale
-                of the solar system. The length of scrolling is the same amount
-                of time it takes light to travel. Mobile version and more
-                effecient rendering is in development!
-              </p>
+              <Collapse isOpen={this.state.collapse}>
+                <Card className="card">
+                  <a href="https://scrollablesolarsystem.netlify.app/">
+                    <CardBody>
+                      <p className="cardText">
+                        Educational React application that helps teach the large
+                        scale of the solar system. The length of scrolling is
+                        the same amount of time it takes light to travel. Mobile
+                        version and more effecient rendering is in development!
+                      </p>
+
+                      <p className="cardText">Click to visit!</p>
+                    </CardBody>
+                  </a>
+                </Card>
+              </Collapse>
             </div>
             <div className="col">
               <h1 className="website">Scrollable Solar System</h1>
 
-              <a href="https://scrollablesolarsystem.netlify.app/">
+              <Button outline onClick={this.handleOpenCard}>
                 <img
-                  className="spishstories"
                   src="/images/scrollablesolarsystem.png"
                   alt="scrollable solar system"
                   height={400}
                   width={500}
                 />
-              </a>
+              </Button>
             </div>
           </div>
           <div className="row">
             <div className="col">
               <h1 className="website">SpishStories.com</h1>
-              <a href="http://spishstories.com">
+              <Button outline onClick={this.handleOpenCard}>
                 <img
-                  className="spishstories"
                   src="/images/spishstories.png"
                   alt="spishstories"
                   height={400}
                   width={500}
                 />
-              </a>
+              </Button>
             </div>
             <div className="col mt-5">
-              <p className="text">
-                React application which hosts stories I have written. Currently
-                using React, CSS, and HTML. A responsive server is in
-                development for a responsive webcomic reader!
-              </p>
+              <Collapse isOpen={this.state.collapse}>
+                <Card className="card">
+                  <a href="https://spishstories.com">
+                    <CardBody>
+                      <p className="cardText">
+                        React application which hosts stories I have written.
+                        Currently using React, CSS, and HTML. A responsive
+                        server is in development for a responsive webcomic
+                        reader!
+                      </p>
+
+                      <p className="cardText">Click to visit!</p>
+                    </CardBody>
+                  </a>
+                </Card>
+              </Collapse>
             </div>
           </div>
         </div>
