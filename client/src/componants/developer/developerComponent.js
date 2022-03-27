e413a90645032e84626335685c0d5f7dea7a3292
spish-web <nickolas.spisak@gmail.com>
2022-03-27 00:18:27 -0400

    final card changes

--- client/src/componants/developer/developerComponent.js
+++ client/src/componants/developer/developerComponent.js
@@ -32,94 +32,86 @@ class Developer extends Component {
           <div className="row">
             <div className="col mb-5">
               <h1 className="website">Posts App</h1>
-              <Button outline onClick={() => this.toggle}>
-                <img
-                  src="/images/posts.png"
-                  alt="posts"
-                  height={400}
-                  width={500}
-                  onClick={this.handleOpenCard}
-                />
-              </Button>
+
+              <img
+                className="spishstories"
+                src="/images/posts.png"
+                alt="posts"
+                height={400}
+                width={500}
+                onClick={this.handleOpenCard}
+              />
             </div>
             <div className="col mt-5">
-              <Collapse isOpen={this.state.collapse}>
-                <Card className="card">
-                  <a href="https://nickspisakposts.netlify.app">
-                    <CardBody>
-                      <p className="cardText">
-                        A responsive post application using React, MongoDB,
-                        Express, Node.js. Responsive account creation, as well
-                        as Google sign in. Each user can create and delete their
-                        own posts. A Like button is currently in development!
-                      </p>
+              <Card className="card">
+                <a href="https://nickspisakposts.netlify.app">
+                  <CardBody>
+                    <p className="cardText">
+                      A responsive post application using React, MongoDB,
+                      Express, Node.js. Responsive account creation, as well as
+                      Google sign in. Each user can create and delete their own
+                      posts. A Like button is currently in development!
+                    </p>
 
-                      <p className="cardText">Click to visit!</p>
-                    </CardBody>
-                  </a>
-                </Card>
-              </Collapse>
+                    <p className="cardText">Click to visit!</p>
+                  </CardBody>
+                </a>
+              </Card>
             </div>
             <div className="col-6 mt-5">
-              <Collapse isOpen={this.state.collapse}>
-                <Card className="card">
-                  <a href="https://scrollablesolarsystem.netlify.app/">
-                    <CardBody>
-                      <p className="cardText">
-                        Educational React application that helps teach the large
-                        scale of the solar system. The length of scrolling is
-                        the same amount of time it takes light to travel. Mobile
-                        version and more effecient rendering is in development!
-                      </p>
+              <Card className="card">
+                <a href="https://scrollablesolarsystem.netlify.app/">
+                  <CardBody>
+                    <p className="cardText">
+                      Educational React application that helps teach the large
+                      scale of the solar system. The length of scrolling is the
+                      same amount of time it takes light to travel. Mobile
+                      version and more effecient rendering is in development!
+                    </p>
 
-                      <p className="cardText">Click to visit!</p>
-                    </CardBody>
-                  </a>
-                </Card>
-              </Collapse>
+                    <p className="cardText">Click to visit!</p>
+                  </CardBody>
+                </a>
+              </Card>
             </div>
             <div className="col">
               <h1 className="website">Scrollable Solar System</h1>
 
-              <Button outline onClick={this.handleOpenCard}>
-                <img
-                  src="/images/scrollablesolarsystem.png"
-                  alt="scrollable solar system"
-                  height={400}
-                  width={500}
-                />
-              </Button>
+              <img
+                className="spishstories"
+                src="/images/scrollablesolarsystem.png"
+                alt="scrollable solar system"
+                height={400}
+                width={500}
+              />
             </div>
           </div>
           <div className="row">
             <div className="col">
               <h1 className="website">SpishStories.com</h1>
-              <Button outline onClick={this.handleOpenCard}>
-                <img
-                  src="/images/spishstories.png"
-                  alt="spishstories"
-                  height={400}
-                  width={500}
-                />
-              </Button>
+
+              <img
+                className="spishstories"
+                src="/images/spishstories.png"
+                alt="spishstories"
+                height={400}
+                width={500}
+              />
             </div>
             <div className="col mt-5">
-              <Collapse isOpen={this.state.collapse}>
-                <Card className="card">
-                  <a href="https://spishstories.com">
-                    <CardBody>
-                      <p className="cardText">
-                        React application which hosts stories I have written.
-                        Currently using React, CSS, and HTML. A responsive
-                        server is in development for a responsive webcomic
-                        reader!
-                      </p>
+              <Card className="card">
+                <a href="https://spishstories.com">
+                  <CardBody>
+                    <p className="cardText">
+                      React application which hosts stories I have written.
+                      Currently using React, CSS, and HTML. A responsive server
+                      is in development for a responsive webcomic reader!
+                    </p>
 
-                      <p className="cardText">Click to visit!</p>
-                    </CardBody>
-                  </a>
-                </Card>
-              </Collapse>
+                    <p className="cardText">Click to visit!</p>
+                  </CardBody>
+                </a>
+              </Card>
             </div>
           </div>
         </div>
